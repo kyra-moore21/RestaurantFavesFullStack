1c0601bd0cb3f0120584305ac4acf997cbf9f808
kyra-moore21 <kyra.moore21@icloud.com>
2024-06-23 21:29:28 -0400

    t commit --amend --author="Your GitHub Username <your-email@example.com>" git rebase --continue first commit first commit

---
+++ Restaurant Backend/Restaurant Backend/Controllers/OrderController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant_Backend.Models;
+
+namespace Restaurant_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        FavsDbContext dbContext = new FavsDbContext();
+
+        //api/Order
+        [HttpGet()]
+        public IActionResult GetAll(string? q = null, bool? orderAgain = null)
+        {
+            List<Order> result = dbContext.Orders.ToList();
+
+            if (q != null)
+            {
+                result = result.Where(b => b.Restaurant.ToLower().Contains(q.ToLower())).ToList();
+            }
+            if (orderAgain != null)
+            {
+                result = result.Where(b => b.OrderAgain == orderAgain).ToList();
+            }
+
+            return Ok(result);
+        }
+
+        //api/Order/id
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            Order result = dbContext.Orders.Find(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        //api/Orders/
+        [HttpPost()]
+        public IActionResult AddOrder([FromBody] Order newOrder)
+        {
+            newOrder.Id = 0;
+            dbContext.Orders.Add(newOrder);
+            dbContext.SaveChanges();
+            return Created($"/api/Books/{newOrder.Id}", newOrder);
+
+        }
+        //api/Orders/{id}
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteOrder(int id)
+        {
+            Order result = dbContext.Orders.Find(id);
+            if (result == null) { return NotFound(); }
+            dbContext.Orders.Remove(result);
+            dbContext.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook([FromBody] Order targetOrder, int id)
+        {
+            if (targetOrder.Id != id) { return BadRequest(); }
+            if (!dbContext.Orders.Any(b => b.Id == id)) { return NotFound(); }
+            dbContext.Orders.Update(targetOrder);
+            dbContext.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ Restaurant Backend/Restaurant Backend/Models/FavsDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Restaurant_Backend.Models;
+
+public partial class FavsDbContext : DbContext
+{
+    public FavsDbContext()
+    {
+    }
+
+    public FavsDbContext(DbContextOptions<FavsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=FavsDB; Integrated Security=SSPI;Encrypt=false;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Order__3214EC07D2E41A50");
+
+            entity.ToTable("Order");
+
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.Restaurant).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
